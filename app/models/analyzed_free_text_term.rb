c98d6144de2b3cf1dfd510303eb2b8fefeeb2768
tibbs001 <sheri.tibbs@duke.edu>
2017-11-10 17:15:50 -0500

    Fix values that go into year validation field

--- app/models/analyzed_free_text_term.rb
+++ app/models/analyzed_free_text_term.rb
@@ -4,6 +4,7 @@ class AnalyzedFreeTextTerm < ActiveRecord::Base
 
   def self.populate_from_file(file_name, year)
     File.open(file_name).each_line{|line|
+      return if line.blank?
       line_array=line.split('|')
       term=line_array[0].split.map(&:capitalize).join(' ').strip
 
@@ -11,14 +12,19 @@ class AnalyzedFreeTextTerm < ActiveRecord::Base
         new(:term=>term,
             :downcase_term=>term.downcase,
             :year=>year,
-            :year_verification=>line_array.last
+            :year_verification=>get_year_verification(line_array, year)
         ).save
 
         CategorizedTerm.create_for(line_array, year, 'free')
 
       end
     }
+  end
 
+  def self.get_year_verification(line, year)
+    return nil if year == '2010'
+    val=line.last.gsub(/\n/,"").gsub(/\r/,"").strip
+    return val if val == 'New only' || val == 'Old only' or val == 'Both'
   end
 
 end
--- app/models/analyzed_mesh_term.rb
+++ app/models/analyzed_mesh_term.rb
@@ -3,9 +3,8 @@ class AnalyzedMeshTerm < ActiveRecord::Base
   has_many :categorized_terms, :foreign_key => 'identifier', :dependent => :delete_all
 
   def self.populate_from_file(file_name, year)
-    puts "about to populate table of analyzed mesh terms..."
-    contents=File.open(file_name)
-    contents.each_line{|line|
+    File.open(file_name).each_line{|line|
+      return if line.blank?
       line_array=line.split('|')
       qualifier=line_array.first.split('.').first
       term=line_array[1].strip
@@ -17,11 +16,17 @@ class AnalyzedMeshTerm < ActiveRecord::Base
             :downcase_term=>term.downcase,
             :term=>term,
             :year=>year,
-            :year_verification=>line_array.last
+            :year_verification=>get_year_verification(line_array, year)
         ).save
         CategorizedTerm.create_for(line_array, year, 'mesh')
       end
     }
+  end
+
+  def self.get_year_verification(line, year)
+    return nil if year == '2010'
+    val=line.last.gsub(/\n/,"").gsub(/\r/,"").strip
+    return val if val == 'New only' || val == 'Old only' or val == 'Both'
   end
 
 end
