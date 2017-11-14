92d692acd2360b575b8d6ab8acea19c7cc01fdd1
tibbs001 <sheri.tibbs@duke.edu>
2017-11-13 19:44:29 -0500

    Simplify clinical_category to just category

--- spec/models/analyzed_free_text_term_spec.rb
+++ spec/models/analyzed_free_text_term_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe AnalyzedFreeTextTerm, type: :model do
       file_name="spec/support/files/#{category.downcase}_free_text_terms.csv"
       AnalyzedFreeTextTerm.populate_from_file(file_name, year)
       expect(CategorizedTerm.count).to eq(8)
-      expect(CategorizedTerm.where('clinical_category=? and year = ?',category, year).size).to eq(4)
+      expect(CategorizedTerm.where('category=? and year = ?',category, year).size).to eq(4)
       amt=AnalyzedFreeTextTerm.where('term=? and year = ?',sample_term, year)
       expect(amt.size).to eq(1)
     end
