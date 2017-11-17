0f51a7b8ad91a4d7c5f6196d81c581cf1486881c
tibbs001 <sheri.tibbs@duke.edu>
2017-11-17 11:48:37 -0500

    Added a row to MeSH lookup tables that include a term used in other tests: C23.888.592.604.646

--- spec/models/y2010_mesh_term_spec.rb
+++ spec/models/y2010_mesh_term_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe Y2010MeshTerm, type: :model do
     it 'should create a row for each incoming line in the file & not duplicate rows' do
       file_name="spec/support/files/2010_mesh_terms.csv"
       Y2010MeshTerm.populate_from_file(file_name)
-      expect(Y2010MeshTerm.count).to eq(32)
+      expect(Y2010MeshTerm.count).to eq(33)
       #verify no duplication
       Y2010MeshTerm.populate_from_file(file_name)
-      expect(Y2010MeshTerm.count).to eq(32)
+      expect(Y2010MeshTerm.count).to eq(33)
     end
 
   end
--- spec/models/y2016_mesh_term_spec.rb
+++ spec/models/y2016_mesh_term_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe Y2016MeshTerm, type: :model do
     it 'should create a row for each incoming line in the file & not duplicate rows' do
       file_name="spec/support/files/2016_mesh_terms.csv"
       Y2016MeshTerm.populate_from_file(file_name)
-      expect(Y2016MeshTerm.count).to eq(42)
+      expect(Y2016MeshTerm.count).to eq(43)
       #verify no duplication
       Y2016MeshTerm.populate_from_file(file_name)
-      expect(Y2016MeshTerm.count).to eq(42)
+      expect(Y2016MeshTerm.count).to eq(43)
     end
 
   end
