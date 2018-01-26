3257938dc10516b7175f07376882ce04dd35b86c
tibbs001 <sheri.tibbs@duke.edu>
2018-01-26 17:12:45 -0500

    aact-562:  For now, will load local copy of the db and restore the public version format.

--- app/models/updater.rb
+++ app/models/updater.rb
@@ -1,16 +1,18 @@
 class Updater
 
+  attr_accessor :con, :pub_con
+
   def populate
     reload_aact_data
     populate_mesh_tables
     populate_analyzed_mesh_term_tables
     populate_analyzed_free_text_tables
     sanity_checks
+    dump_database
   end
 
   def populate_mesh_tables
     # Load MeSH terms for 2010 & 2016
-    con=ActiveRecord::Base.establish_connection.connection
     con.execute("truncate table y2010_mesh_terms")
     Y2010MeshTerm.populate_from_file
     con.execute("truncate table y2016_mesh_terms")
@@ -45,15 +47,47 @@ class Updater
     AnalyzedFreeTextTerm.populate_from_file(file,'2018')
   end
 
-  def reload_aact_data(dmp_file='/aact-files/other/aact_20170903.psql')
-    db_name=ActiveRecord::Base.connection.current_database
-    cmd="psql -h localhost #{db_name} < #{dmp_file} > /dev/null"
+  def reload_aact_data(dmp_file='/aact-files/other/aact_20170903.dmp')
+    if Rails.env == 'production'
+      host=ENV['CLINCAT_HOSTNAME_PROD']
+    else
+      host='localhost'
+    end
+    cmd="PGPASSWORD=#{ENV['CLINCAT_PASSWORD_PROD']} pg_restore -c -j 5 -v -h #{host} -p 5432 -U #{ENV['DB_SUPER_USERNAME']} -d #{db_name} #{dmp_file}"
     system cmd
     con=ActiveRecord::Base.establish_connection.connection
     con.execute('grant SELECT on all tables in schema public to public')
     current_users.each{|user|
       con.execute("grant connect on database #{db_name} to #{user}")
     }
+  end
+
+  def dump_database
+    psql_file="/aact-files/other/ClinCat_#{Time.now.strftime("%Y%m%d_%H")}.psql"
+    File.delete(psql_file) if File.exist?(psql_file)
+    cmd="PGPASSWORD=#{ENV['DB_SUPER_PASSWORD']} pg_dump --no-owner --no-acl -h localhost -U #{ENV['DB_SUPER_USERNAME']} #{db_name} > #{psql_file}"
+    system cmd
+  end
+
+  def restore_public_database
+
+    # clear out previous content of public db
+    pub_con.execute('DROP SCHEMA IF EXISTS public CASCADE')
+    pub_con.execute('CREATE SCHEMA public')
+    cmd="psql -h aact-db.ctti-clinicaltrials.org #{db_name} < #{psql_file} > /dev/null"
+    system cmd
+  end
+
+  def db_name
+    con.current_database
+  end
+
+  def pub_con
+    @pub_con ||= ActiveRecord::Base.establish_connection(ENV["CLINCAT_PUBLIC_DATABASE_URL"]).connection
+  end
+
+  def con
+    @con ||= ActiveRecord::Base.establish_connection(ENV["CLINCAT_LOCAL_DATABASE_URL"]).connection
   end
 
   def sanity_checks
